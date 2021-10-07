bed90226081b240de6ca6f6fff1721e8ac8dd52b
Emily Baytalsky <ebaytalsky@berkeley.edu>
2021-10-07 00:05:18 -0700

    summary
    summary

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -40,3 +40,14 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+$("[rel='tooltip']").tooltip();
+
+$('.thumbnail').hover(
+    function(){
+        $(this).find('.caption').slideDown(250); //.fadeIn(250)
+    },
+    function(){
+        $(this).find('.caption').slideUp(250); //.fadeOut(205)
+    }
+);
